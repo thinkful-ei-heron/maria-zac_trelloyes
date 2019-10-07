e068dc396cac06fb21aac1aa861470710e6cd780
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-10-07 16:32:23 -0500

    almost there

--- src/App.js
+++ src/App.js
@@ -1,9 +1,67 @@
 import React from 'react';
 
-function App() {
+class AppClass extends React.Component {
+  static defaultProps = {store: {lists:[], allCards: {} } }
+  render() {return;};
+}
+
+function Card(props) {
+  console.log(props);
+  return (
+    <div className="Card">
+      <button type="button">delete</button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  );
+}
+
+function List(props) {
+  console.log(props);
+  const arr = [];
+  for (let i = 0; i < props.cards.length; i++) {
+    arr.push(<Card
+      key={props.cards.id}
+      title={props.cards.title}
+      content={props.cards.content} />);
+  };
+  return (
+    <section className="List">
+        <header className="List-header">
+          <h2>First list</h2>
+        </header>
+        <div className="List-cards">
+            {arr}
+            <button type="button" className="List-add-button">
+              + Add Random Card
+            </button>
+        </div>
+      </section>
+  )
+}
+
+function App(props) {
+  const lists = props.store.STORE.lists;
+  let arr = [];
+  let cards = [];
+  for (let i = 0; i < lists.length; i++) {
+    cards = lists[i].cardIds.map(input => {
+      return props.store.STORE.allCards[input];
+    },);
+    arr.push(
+      <div className="App-list">
+      <List key={lists[i].id} header={lists[i].header} cards={cards} />
+      </div>
+    );
+    cards = [];
+  };
+  
   return (
     <main className='App'>
-      {/* content goes here */}
+      <header className="App-header">
+        <h1>Trelloyes!</h1>
+      </header>
+      {arr}
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import './app.css';
+import './list.css';
+import './card.css';
+import STORE from './store.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={STORE} />, document.getElementById('root'));
